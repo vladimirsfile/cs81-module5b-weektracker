1dc2ca4ce1dce023fab2e7e9cab18f7cdbc2c838
Vladimir Angelov <vladimirsfile@gmail.com>
2026-01-22 18:13:08 +0200

    Add functions to calculate: total hours on physical activity and average afternoon enjoyment.

--- activityTracker.js
+++ activityTracker.js
@@ -8,6 +8,30 @@ const myWeek = [
     {day: "Sunday", activity: "paintball", category: "social", hoursSpent: 2, enjoyment: 7, timeOfDay: "afternoon"}
 ];
 
+
 // I predict that "tennis" will have the highest enjoyment.
 // I predict that the "physical" category will dominate my week. 
 // I predict a pattern in which the physical activities are done predominantly earlier during the day. 
+
+
+// Calculate total hours spent on physical activity.
+function totalPhysicalHours(log) {
+    // First, filter the physical activities.
+    const physicalActivity = log.filter(day => day.category === "physical");
+    // Then calculate their total by using the reduce() function.
+    const totalPhysicalActivities = physicalActivity.reduce((total, day) => total + day.hoursSpent, 0);
+    return totalPhysicalActivities;
+}
+
+// Calculate average enjoyment for afternoon.
+function averageAfternoonEnjoyment(log) {
+    // First, filter out the activies, so we just keep the "afternoon".
+    const afternoonActivities = log.filter(entry => entry.timeOfDay === "afternoon");
+    // Now, create a new array that just has "enjoyment" scores.
+    const scores = afternoonActivities.map(entry => entry.enjoyment);
+    // Lastly, calculate the average. 
+    return scores.reduce((total, score) => total + score, 0) / afternoonActivities.length;
+}
+
+console.log(`Total time spent on physical activities: ${totalPhysicalHours(myWeek)} hours`);
+console.log("Average afternoon enjoyment:", averageAfternoonEnjoyment(myWeek));
