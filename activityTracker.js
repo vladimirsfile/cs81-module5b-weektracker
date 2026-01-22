ff3bf03ed35589229e118698a11ddefdd38b5af3
Vladimir Angelov <vladimirsfile@gmail.com>
2026-01-22 12:01:56 +0200

    Created an array called myWeek containing 7 objects.

---
+++ activityTracker.js
@@ -0,0 +1,10 @@
+const myWeek = [
+    {day: "Monday", activity: "studying", category: "educational", hoursSpent: 4, enjoyment: 5, timeOfDay: "evening"},
+    {day: "Tuesday", activity: "tennis", category: "physical", hoursSpent: 2, enjoyment: 10, timeOfDay: "morning"},
+    {day: "Wednesday", activity: "studying", category: "educational", hoursSpent: 3, enjoyment: 5, timeOfDay: "afternoon"},
+    {day: "Thursday", activity: "fitness", category: "physical", hoursSpent: 1.5, enjoyment: 4, timeOfDay: "afternoon"},
+    {day: "Friday", activity: "going out", category: "social", hoursSpent: 4, enjoyment: 8, timeOfDay: "evening"},
+    {day: "Saturday", activity: "fitness", category: "physical", hoursSpent: 1.5, enjoyment: 4, timeOfDay: "morning"},
+    {day: "Sunday", activity: "paintball", category: "social", hoursSpent: 2, enjoyment: 7, timeOfDay: "afternoon"}
+];
+
